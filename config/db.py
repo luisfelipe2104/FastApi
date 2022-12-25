094d2b41d21a4db42491e3475a21ba33760c86d5
luisfelipe2104 <100540998+luisfelipe2104@users.noreply.github.com>
2022-12-25 20:33:37 -0300

    sqlite3

--- config/db.py
+++ config/db.py
@@ -1,5 +1,24 @@
 from sqlalchemy import create_engine, MetaData
 
-engine = create_engine("mysql+pymysql://root@localhost:3306/fastApi")  # sqlite:///database.db
+# "mysql+pymysql://root@localhost:3306/fastApi"
+
+engine = create_engine("sqlite:///database.sqlite3")  # sqlite:///database.db
+# engine.execute("USE teste")
+# engine.execute("CREATE TABLE IF NOT EXISTS users")
 meta = MetaData()
 conn = engine.connect()
+engine.execute("""CREATE TABLE users (
+      id integer primary key autoincrement,
+     name text,
+      email text,
+      password text
+   );""")
+
+
+"""
+CREATE TABLE tbl2 (
+   ...>   f1 varchar(30) primary key,
+   ...>   f2 text,
+   ...>   f3 real
+   ...> );
+"""
